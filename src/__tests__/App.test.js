6461fcc08e6ebea62d35a1389d53ffbb498f8d34
Queentel <joiqueentel@gmail.com>
2025-07-30 17:55:00 +0300

    add all changes

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,45 +1,66 @@
+import React from "react";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import '@testing-library/jest-dom';
-
 import App from "../App";
 
-// Pepperoni checkbox
-test("checkbox is initially unchecked", () => {
-  render(<App />);
+describe("Pizza Ordering App", () => {
+  test("size select element initially displays 'Small'", () => {
+    render(<App />);
+    const selectSize = screen.getByLabelText(/select size/i);
+    expect(selectSize).toHaveDisplayValue("Small");
+  });
 
-  const addPepperoni = screen.getByRole("checkbox", { name: /add pepperoni/i });
+  test("select Size dropdown displays the user's selected value", () => {
+    render(<App />);
+    const selectSize = screen.getByLabelText(/select size/i);
 
-  expect(addPepperoni).not.toBeChecked();
+    userEvent.selectOptions(selectSize, "medium");
+    expect(selectSize).toHaveDisplayValue("Medium");
+
+    userEvent.selectOptions(selectSize, "large");
+    expect(selectSize).toHaveDisplayValue("Large");
+  });
+
+  test("'Your Selection' message initially displays 'small cheese'", () => {
+    render(<App />);
+    expect(screen.getByText(/small cheese/i)).toBeInTheDocument();
+  });
+
+  test("selecting options updates the 'Your selection' message", () => {
+    render(<App />);
+    const addPepperoni = screen.getByRole("checkbox", { name: /add pepperoni/i });
+    const selectSize = screen.getByLabelText(/select size/i);
+
+    userEvent.click(addPepperoni);
+    expect(screen.getByText(/small pepperoni/i)).toBeInTheDocument();
+
+    userEvent.selectOptions(selectSize, "large");
+    expect(screen.getByText(/large pepperoni/i)).toBeInTheDocument();
+  });
+
+  test("'Contact Info' text box initially displays a placeholder value of 'email address'", () => {
+    render(<App />);
+    expect(screen.getByPlaceholderText(/email address/i)).toBeInTheDocument();
+  });
+
+  test("the page shows information the user types into the contact form field", () => {
+    render(<App />);
+    const contact = screen.getByLabelText(/enter your email address/i);
+    userEvent.type(contact, "pizzafan@email.com");
+    expect(contact).toHaveValue("pizzafan@email.com");
+  });
+
+  test("form contains a 'Submit Order' button", () => {
+    render(<App />);
+    const button = screen.getByRole("button", { name: /submit order/i });
+    expect(button).toBeInTheDocument();
+  });
+
+  test("clicking the Place Order button displays a thank you message", () => {
+    render(<App />);
+    const button = screen.getByRole("button", { name: /submit order/i });
+    userEvent.click(button);
+    expect(screen.getByText(/thanks for your order!/i)).toBeInTheDocument();
+  });
 });
 
-test("checkbox appears as checked when user clicks it", () => {
-  render(<App />);
-
-  const addPepperoni = screen.getByRole("checkbox", { name: /add pepperoni/i });
-
-  userEvent.click(addPepperoni);
-  expect(addPepperoni).toBeChecked();
-});
-
-test("checkbox appears as unchecked when user clicks a second time", () => {
-  render(<App />);
-
-  const addPepperoni = screen.getByRole("checkbox", { name: /add pepperoni/i });
-
-  userEvent.click(addPepperoni);
-
-  expect(addPepperoni).toBeChecked();
-
-  userEvent.click(addPepperoni);
-
-  expect(addPepperoni).not.toBeChecked();
-});
-
-// Size select element
-
-// "Your Selection" text
-
-// "Contact Info" text box
-
-// Submit Order button
